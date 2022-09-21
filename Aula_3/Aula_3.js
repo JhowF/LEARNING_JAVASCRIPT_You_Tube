b63b8e013cc82c4fa10375c2ff760b3c73525ad5
JhowF <jonathanferreirar@gmail.com>
2022-09-21 00:28:15 -0300

    Aula_3_sobre_a_linguagem

---
+++ Aula_3/Aula_3.js
@@ -0,0 +1,34 @@
+              /*SOBRE A LINGUAGEM*/
+
+//_O codigo é execultado de cima para baixo;
+
+console.log('Primeira parte a ser execultada');
+console.log('Segudna parte a ser execultada');
+console.log('Terceira parte a ser execultada');
+
+//_A linguagem é case sensitive;
+
+//console.LOG("Vai dar erro pos não existe essa função em maiusculo")
+console.log('Maneira correta');
+
+//_Tipagem Fraca;
+
+var a = 1;
+a = 'teste';
+
+console.log(a);
+
+//_O ponto e vírgula a cada instrução é opcional; *  "Há exeção"
+
+console.log(a)   //Funciona da msm maneira
+
+
+
+//_Há comentários para uma linha e múltiplas linhas;
+
+//Maneira de comentar uma linha
+
+/* Maneira de comentar
+múltiplas 
+linhas
+*/
