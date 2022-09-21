4657989156440de2dc50760bcdcaa5aea1caa9a7
JhowF <jonathanferreirar@gmail.com>
2022-09-21 00:42:48 -0300

    Aula_4_variaveis

---
+++ Aula_4/Aula_4.js
@@ -0,0 +1,30 @@
+//VARIÁVEIS
+
+//Só posso usar $ é _ com caracteres especiais no inicio das minhas variaveis
+
+var nome = ("Jonathan R.")   ///Maneira padrão
+
+var nome = ("Paloma F.")   //Posso declarar mais de uma vez a mesma variavel mudando ela o que pode ser ruim
+
+console.log(nome)
+
+let myName = ("Jonathan R.")   //Não me deixa declarar a mesma variavel mais de uma vez
+
+myName = ('Paloma .F')
+
+console.log(myName)
+
+
+const MyName = ("Jonathan R.")  //Quando declaro uma variavel como const eu não posso alterar seu valor
+
+//MyName = ("Paloma F.")     //Está dando erro pois estou tentando alterar a minha variavel 
+
+
+console.log(MyName)
+
+
+let criandoVarVazia;
+
+console.log(criandoVarVazia)
+
+
