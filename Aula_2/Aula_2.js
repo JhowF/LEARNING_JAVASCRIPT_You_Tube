f3c4993d2311b189bde1c5da69b12934807bd1ee
JhowF <jonathanferreirar@gmail.com>
2022-09-21 00:10:59 -0300

    Aula_2_como_inserir_js_no_html

---
+++ Aula_2/Aula_2.js
@@ -0,0 +1,10 @@
+// Formas de inserir arquivo js no html
+
+//head: Linkando um arquivo de scrip;
+
+//body: Escrevendo código entre as tags scripts;
+
+//body: Linkando um arquivo de script;
+
+console.log('Script no header!')
+
