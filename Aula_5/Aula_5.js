cc657682acf3c82df690fe78991a92c6d006f8bf
JhowF <jonathanferreirar@gmail.com>
2022-09-22 23:33:01 -0300

    Aula_5_tipos_de_dados

---
+++ Aula_5/Aula_5.js
@@ -0,0 +1,22 @@
+//TIPOS DE DADOS
+
+// Number, String, Boolean, Num e Undefined, Oobject:
+
+//NaN _ Not a number
+
+
+let numero = 5;
+
+console.log(numero);
+console.log(typeof numero);
+
+const nu_float = 5.32;
+
+console.log(nu_float);
+console.log(typeof nu_float);
+
+
+var nutext = '72';
+
+console.log(nutext);
+console.log(typeof nutext);
