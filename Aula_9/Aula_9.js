cd5cd2b3bfb39f66b271f20fed65e15cddd76d8f
JhowF <jonathanferreirar@gmail.com>
2022-09-23 00:12:32 -0300

    Aula_9_objetos

--- Aula_9/Aula_9.js
+++ Aula_9/Aula_9.js
@@ -1,1 +1,24 @@
 //Dado : Objetos
+
+// Funciona como um array associativo de outras linguagens;
+
+
+
+//São similiares a dicionarios em Python
+let obj ={//As chaves determinam que é um objeto
+    
+    Nome: 'Jonathan',
+    Idade: 29,
+    Profissao: 'Programador'
+
+};
+
+// console.log(obj)
+
+// console.log(typeof obj)
+
+console.log(obj.Nome)
+console.log(obj.Idade)
+console.log(obj.Profissao)
+
+console.log('Meu nome é ' + obj.Nome + 'e minha idade é ' + obj.Idade + ' Anos');
