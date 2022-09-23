bff646bfdca59c881f0f9d453ea520f9ca77dd55
JhowF <jonathanferreirar@gmail.com>
2022-09-22 23:50:45 -0300

    Aula_7_boolean

---
+++ Aula_7/Aula_7.js
@@ -0,0 +1,18 @@
+//BOOLEAN
+
+//Comparações resultam em booleans;
+
+let verdade = true;
+
+console.log(verdade);
+console.log(typeof verdade);
+
+console.log('------------------------------------------------------------------------------------');
+console.log(typeof true);
+console.log(typeof false);
+console.log('------------------------------------------------------------------------------------');
+
+let falso = false;
+
+console.log(falso);
+console.log(typeof falso);
