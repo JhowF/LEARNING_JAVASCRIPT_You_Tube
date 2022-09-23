7da9ed3e8a2c95f49e623d9d5c1db8d23b36db63
JhowF <jonathanferreirar@gmail.com>
2022-09-23 00:04:08 -0300

    Aula_8_undefined_null

---
+++ Aula_8/Aula_8.js
@@ -0,0 +1,25 @@
+//Undefined e null
+
+//Null = Tipos de dado que representa um valor;
+//Undefined = Tipo de dado para variável com valor não atribuído;
+
+
+console.log(sobreNome);         // Só fica undefined e só funciona com var
+
+let nome = null;     //null
+var sobreNome =   //undefined
+
+
+
+console.log(nome);
+console.log(typeof nome);
+console.log(sobreNome);
+
+nome = 'Jonathan';
+sobreNome = 'Rodrigo';
+
+console.log(nome);
+console.log(typeof nome);
+console.log(sobreNome);
+
+
