84ea8c5dc9ac30580127ae3a3e40b1bb7f790c17
JhowF <jonathanferreirar@gmail.com>
2022-09-22 23:44:29 -0300

    Aula_6_string

--- Aula_6/Aula_6.js
+++ Aula_6/Aula_6.js
@@ -2,3 +2,15 @@
 
 // Assim como Python aceita aspas simples e duplas! ' ou "😊👌
 
+
+const nome = "Jonathan";
+const sobreNome = 'Rodrigo';
+
+console.log(nome);
+console.log(typeof nome);
+console.log(nome +" "+ sobreNome);  //Caso prefira poderia simplismente dar um espaço no no R após as aspas.
+
+
+//document.write("Essa frrase aqui, ira aparecer no meu site linkado ao Javascript");
+
+console.log('"Para ter aspas no texto a envolva por outra aspa"');
