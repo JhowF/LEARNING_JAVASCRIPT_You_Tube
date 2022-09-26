e938e73f7947189549f133df7dfe5c8d51ce8dd2
JhowF <jonathanferreirar@gmail.com>
2022-09-26 00:27:09 -0300

    Aula_10_Arreys_exemplos

--- Aula_10/Aula_10.js
+++ Aula_10/Aula_10.js
@@ -1,2 +1,16 @@
 //Arrays
 
+//Na verdade Arrey em Js são objetos porem servem como listas;
+
+let arr = [5, "Matheus", true, {Idade: 18, Nascimento:2003}];
+
+let tamanho = arr.length;
+console.log(arr)
+console.log(arr[tamanho-1])
+
+
+
+
+
+
+
