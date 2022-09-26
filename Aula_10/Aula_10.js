97ca96ec8e1dcd7b30f8bb0c69bec9537f714dd6
JhowF <jonathanferreirar@gmail.com>
2022-09-26 00:32:12 -0300

    Aula_10_Arreys_finish

--- Aula_10/Aula_10.js
+++ Aula_10/Aula_10.js
@@ -5,9 +5,19 @@
 let arr = [5, "Matheus", true, {Idade: 18, Nascimento:2003}];
 
 let tamanho = arr.length;
-console.log(arr)
-console.log(arr[tamanho-1])
+// console.log(arr)
+// console.log(arr[tamanho-1])
 
+
+arr [0] = "Jonathan";
+arr [2] = "Rodrigo";
+
+// console.log(arr)
+
+
+arr[tamanho] = "Paloma"; //Adicionando argumento no ultimo valor
+
+console.log(arr)
 
 
 
