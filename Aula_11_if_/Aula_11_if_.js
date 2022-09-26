04d40c71bfc0b7ecb08a4a24bbaf32472c9d1436
JhowF <jonathanferreirar@gmail.com>
2022-09-26 00:33:42 -0300

    nome_arquivos_alterados

---
+++ Aula_11_if_/Aula_11_if_.js
@@ -0,0 +1,26 @@
+//Arrays
+
+//Na verdade Arrey em Js são objetos porem servem como listas;
+
+let arr = [5, "Matheus", true, {Idade: 18, Nascimento:2003}];
+
+let tamanho = arr.length;
+// console.log(arr)
+// console.log(arr[tamanho-1])
+
+
+arr [0] = "Jonathan";
+arr [2] = "Rodrigo";
+
+// console.log(arr)
+
+
+arr[tamanho] = "Paloma"; //Adicionando argumento no ultimo valor
+
+console.log(arr)
+
+
+
+
+
+
