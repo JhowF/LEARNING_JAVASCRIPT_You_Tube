7c1718a15a476c00c0ead7497d0569bcfb371eeb
JhowF <jonathanferreirar@gmail.com>
2022-09-26 00:45:35 -0300

    Aula_11_if_aprendendo

--- Aula_11_if_/Aula_11_if_.js
+++ Aula_11_if_/Aula_11_if_.js
@@ -3,9 +3,22 @@
 
 /*Operadores condicionais */
 
+if (true) {
+    console.log("O codigo aqui dentro será execultado!");
+}
+
+
+n= 0
+
+if (n < 10) {
+    console.log("Aprendendo")
+    
+}
 
 
 
-
-
+while(n < 10) {
+    console.log(`${n+1}. Aprendendo While`)
+    n+=1
+}
 
