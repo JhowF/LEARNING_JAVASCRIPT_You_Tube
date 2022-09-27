fe26e9c16877b7432be7f2c2f20ecfe6ca8283e7
JhowF <jonathanferreirar@gmail.com>
2022-09-27 00:04:33 -0300

    Aula_14_Comparação_tipo_e_valor_testes

--- Aula_14_Comparação_tipo_e_valor/Aula_14_Comp_tipo_e_valor.js
+++ Aula_14_Comparação_tipo_e_valor/Aula_14_Comp_tipo_e_valor.js
@@ -3,3 +3,41 @@
 //Operadores:    === e !==;
 
 //Função é compara valor e tipo de dado;
+
+let numero = 10;
+
+if (numero == 'dez'){
+    console.log('Sucesso Teste (1)')
+}else{
+    console.log('Falha(1)')
+}
+console.log('- + - + - + -')
+if (numero == '10'){
+    console.log('Sucesso Teste (2)')
+}else{
+    console.log('Falha(2)')
+}
+console.log('- + - + - + -')
+if (numero == 10){
+    console.log('Sucesso Teste (3)')
+}else{
+    console.log('Falha(3)')
+}
+console.log('- + - + - + -')
+if (numero === '10'){
+    console.log('Sucesso Teste (4)')
+}else{
+    console.log('Falha(4)')
+}
+console.log('- + - + - + -')
+if (numero === 'dez'){
+    console.log('Sucesso Teste (5)')
+}else{
+    console.log('Falha(5)')
+}
+console.log('- + - + - + -')
+if (numero === 10){
+    console.log('Sucesso Teste (6)')
+}else{
+    console.log('Falha(6)')
+}
