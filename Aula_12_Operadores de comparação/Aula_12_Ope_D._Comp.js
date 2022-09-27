10604251b5636b52510e3c5a55761fc8d43bcd0d
JhowF <jonathanferreirar@gmail.com>
2022-09-26 23:21:46 -0300

    Aula_12_Operadores de comparação

---
+++ Aula_12_Operadores de comparação/Aula_12_Ope_D._Comp.js
@@ -0,0 +1,21 @@
+//Operadores de comparação
+
+//Operadores:  ==, !=, >, <, >=, <=
+
+//A parti dessas expreções podemos obter um true ou false;
+
+
+// ==  _Igual 
+
+// !=  _Diferente
+
+// >   _Maior
+ 
+// <   _Menor
+
+// >=  _Maior ou igual
+
+// <=  _Menor ou igual
+
+
+ 
