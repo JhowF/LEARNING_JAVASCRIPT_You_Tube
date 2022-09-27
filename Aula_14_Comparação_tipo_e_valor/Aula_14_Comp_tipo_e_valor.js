aea77ebb62b64f4264e0c4ed4ea1df5f3bc0454d
JhowF <jonathanferreirar@gmail.com>
2022-09-27 00:09:49 -0300

    Aula_14_Comparação_tipo_e_valor_finish

--- Aula_14_Comparação_tipo_e_valor/Aula_14_Comp_tipo_e_valor.js
+++ Aula_14_Comparação_tipo_e_valor/Aula_14_Comp_tipo_e_valor.js
@@ -6,38 +6,69 @@
 
 let numero = 10;
 
+//1
 if (numero == 'dez'){
     console.log('Sucesso Teste (1)')
 }else{
     console.log('Falha(1)')
 }
 console.log('- + - + - + -')
+
+//2
 if (numero == '10'){
     console.log('Sucesso Teste (2)')
 }else{
     console.log('Falha(2)')
 }
 console.log('- + - + - + -')
+
+//3
 if (numero == 10){
     console.log('Sucesso Teste (3)')
 }else{
     console.log('Falha(3)')
 }
 console.log('- + - + - + -')
+
+//4
 if (numero === '10'){
     console.log('Sucesso Teste (4)')
 }else{
     console.log('Falha(4)')
 }
 console.log('- + - + - + -')
+
+//5
 if (numero === 'dez'){
     console.log('Sucesso Teste (5)')
 }else{
     console.log('Falha(5)')
 }
 console.log('- + - + - + -')
+
+//6
 if (numero === 10){
     console.log('Sucesso Teste (6)')
 }else{
     console.log('Falha(6)')
+}
+
+
+
+
+
+//7
+
+if (numero != '10'){
+    console.log('Valor diferente (7)')
+}else{
+    console.log('Valor igual (7)')
+}
+
+
+//8
+if (numero !== '10'){
+    console.log('Valor diferente (8)')
+}else{
+    console.log('Valor igual (8)')
 }
