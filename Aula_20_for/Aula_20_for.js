614c53a3372062a7e6a9b04b869e8a93da1b5155
JhowF <jonathanferreirar@gmail.com>
2022-09-29 00:00:54 -0300

    Aula_20_for_finish

--- Aula_20_for/Aula_20_for.js
+++ Aula_20_for/Aula_20_for.js
@@ -1,1 +1,36 @@
 //FOR
+
+//For é feito em somente uma linha
+
+//for (variavel; condição/incremento){
+
+//}
+
+for (i=0;i < 10;i++){
+    console.log(`${i+1} _ it worked out`);
+}
+
+
+console.log('')
+
+n = 0
+for (y=0;y < 20; y++){
+    n++
+    console.log(`n = ${n}+${n} = ${ n+n}`);
+
+}
+
+console.log('')
+
+var arr = [4,3,2,1]
+
+for(var j = 0; j < arr.length; j++){
+    console.log(`${arr[j]}`)
+}
+
+
+console.log('')
+
+for (t = 5; t < 100; t *=3){
+    console.log(t)
+}
