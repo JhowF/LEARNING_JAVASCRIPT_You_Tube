57d9258b33840728fdd76a2658acfb3b22d613c5
JhowF <jonathanferreirar@gmail.com>
2022-09-28 23:02:00 -0300

    Aula_18_while_

--- Aula_18_while_for/Aula_18_while_for.js
+++ Aula_18_while_for/Aula_18_while_for.js
@@ -1,1 +1,46 @@
 //WHILE         FOR
+
+/* while (){
+
+} */
+
+
+let x = 0;
+
+while (x < 5){
+    console.log(`${x+1} _ Testando while`);
+
+    x++
+    //x+=1
+
+}
+
+
+let arr =  ['Jonathan', 'Paloma', 'Wagner', 'Carlos']
+
+console.log('')
+
+var y=0
+while(y <= 3){
+
+    console.log(`${y+1} _ ${arr[y]}`)
+
+    y++
+}
+
+
+
+//NO JS PODEMOS ITERAR ENTRE CADA PALAVRA
+
+console.log('')
+
+let nome = "Jonathan.R";
+
+let i = 0;
+
+while(i != 10) {
+
+    console.log(nome[i])
+
+    i++
+}
