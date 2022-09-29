9364723a62bb9b0953059739a06ae8605ffe652f
JhowF <jonathanferreirar@gmail.com>
2022-09-28 23:22:09 -0300

    Operadores_de_atribuição_finish

--- Aula_19_Operadores_de_atribuição/Aula_19_Operadores_de_atribuição.js
+++ Aula_19_Operadores_de_atribuição/Aula_19_Operadores_de_atribuição.js
@@ -4,3 +4,54 @@
 //-= || --  MENOS 
 //*=   VEZES 
 ///=   DIVISÃO
+
+
+
+var x =1;
+var y =  2;
+
+console.log(x = x + y );
+
+var x =1;
+var y =  2;
+console.log(x += y );
+
+
+var x =1;
+var y =  2;
+console.log(x -= y );
+
+
+var x =1;
+var y =  2;
+console.log(x *= y );
+
+
+
+var x =1;
+var y =  2;
+console.log(x /= y );
+
+
+
+
+
+
+var x =1;
+
+console.log(x++ );
+
+
+var x =1;
+
+console.log(++x );
+
+
+var x =1;
+
+console.log(x-- );
+
+var x =1;
+
+console.log(--x );
+
