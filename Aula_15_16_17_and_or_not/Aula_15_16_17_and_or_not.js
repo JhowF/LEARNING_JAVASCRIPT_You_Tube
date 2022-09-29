ab8ea363eafd1a79c99950da634c3682e7f9461f
JhowF <jonathanferreirar@gmail.com>
2022-09-28 22:32:39 -0300

    Aula_15_16_17_and_or_not

---
+++ Aula_15_16_17_and_or_not/Aula_15_16_17_and_or_not.js
@@ -0,0 +1,19 @@
+//AND = &&    OR = ||    NOT = !
+
+
+var idade = 12;
+let nome = 'Jhow'
+
+if (numero != '10'){
+    console.log('Valor diferente (7)')
+}else{
+    console.log('Valor igual (7)')
+}
+
+
+//8
+if (numero !== '10'){
+    console.log('Valor diferente (8)')
+}else{
+    console.log('Valor igual (8)')
+}
