04b6aef58c31567b172e1342fcf65dbaed84e175
JhowF <jonathanferreirar@gmail.com>
2022-09-29 22:00:44 -0300

    Aula_21_break

--- Aula_21_break_continue/Aula_21_break_continue.js
+++ Aula_21_break_continue/Aula_21_break_continue.js
@@ -1,1 +1,26 @@
 //Break e continue
+
+//Break  Podemos usar para encerrar um estrutura
+
+//Continue Podemos pular instrução especifica  _ Apos o continue ele não vai execultar os codigos abaixo do mesmo pulando assim para a proxima execulção
+
+//Normalmente usado em loops
+
+
+for (let i = 10; i > 0; i--){
+    
+    console.log(`${i} _ Execução`)
+    
+   
+    
+    if(i===5){
+        break;
+    }
+
+}
+console.log('')
+
+console.log(`Break`)
+
+
+
