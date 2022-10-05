0a891f41ea2656d2fd411fe84299a693a504adde
JhowF <jonathanferreirar@gmail.com>
2022-10-04 23:08:41 -0300

    Aula_26_Métodos_string_finish_part_1

--- Aula_26_Métodos_string/Aula_26_Métodos_string.js
+++ Aula_26_Métodos_string/Aula_26_Métodos_string.js
@@ -1,1 +1,24 @@
 //MÉTODOS DE STRING
+var frase = "Jonathan é brabo"
+var nome =  'Jonathan';
+
+//length
+console.log(nome.length);
+
+//indedxOf()
+console.log(nome.indexOf('n'));
+console.log(frase.indexOf('é'));
+
+//slice   _Primeiro numero é o argumento que começa e o segundo é o argumento que para
+console.log(nome.slice(0,4))
+console.log(frase.slice(11))
+
+//replace
+var novafrase = frase.replace('brabo', 'Lindo');
+console.log(novafrase);
+
+//toLowerCase     _Coloca em Minúsculo
+console.log(novafrase.toLowerCase())
+
+//toUpperCase     _Coloca em Maiúsculo
+console.log(novafrase.toUpperCase())
