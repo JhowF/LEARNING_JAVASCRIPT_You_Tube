ad43c682aba1926d1ea2f25b7d226b3e1932eb2b
JhowF <jonathanferreirar@gmail.com>
2022-10-04 23:42:10 -0300

    Aula_27_Funções_Arreys_parte_1_pop_push_shift...

--- Aula_27_Funções_Arrays/Aula_27_Funções_Arrays.js
+++ Aula_27_Funções_Arrays/Aula_27_Funções_Arrays.js
@@ -1,1 +1,28 @@
 //MÉTODOS DE ARRAYS
+
+var arr = [1,2,3,4,5]
+
+//length
+console.log(arr.length);
+
+// push  _Adiciona na ultima posição
+arr.push(6);
+console.log(arr);
+
+// pop  Remove na ultima posição
+arr.pop();
+console.log(arr);
+
+// unshift  _Adiciona na primeira posição
+arr.unshift('Teste');
+console.log(arr);
+
+// shift   _Remove na primeira posição
+arr.shift();
+console.log(arr);
+
+//Acessar o ultimo elemento
+console.log(arr.length -1);
+
+//Verificar array
+console.log(Array.isArray(arr));
