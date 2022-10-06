f49853cacca3aef7a8842f6fae0fe5db76c87b4b
JhowF <jonathanferreirar@gmail.com>
2022-10-06 00:43:21 -0300

    Aula_29_this_finish

--- Aula_29_this/Aula_29_this.js
+++ Aula_29_this/Aula_29_this.js
@@ -1,3 +1,26 @@
 //THIS
 
 //Fora de escopos locais o this vai sempre se referir ao objeto global Window
+
+//console.log(this);
+
+let teste = 'testando';
+console.log(this.teste);
+
+let pessoa = {
+    nome: 'Jonathan',
+    Idade: 21,
+    falar: function(){
+        console.log('Olá tudo bem ?');
+        
+    },
+    soma: function(a, b){
+        console.log(a+b);
+    },
+    dizerNome: function(){
+          console.log('Meu nome é ' + this.nome);
+      console.log('Minha idade é ' + this.Idade+ ' Anos');
+}
+}
+
+pessoa.dizerNome()
