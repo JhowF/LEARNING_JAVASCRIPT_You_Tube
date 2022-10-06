007913afada96f30f9a67fc49489e710f483a65f
JhowF <jonathanferreirar@gmail.com>
2022-10-06 00:09:04 -0300

    Aula_28_Objetos_com_métodos_finish

--- Aula_28_Objetos_com_métodos/Aula_28_Objetos_com_métodos.js
+++ Aula_28_Objetos_com_métodos/Aula_28_Objetos_com_métodos.js
@@ -1,1 +1,16 @@
 //CRIANDO OBJETOS COM MÉTODOS
+
+let pessoa = {
+    nome: 'Jonathan',
+    Idade: 21,
+    falar: function(){
+        console.log('Olá tudo bem ?');
+        
+    },
+    soma: function(a, b){
+        console.log(a+b);
+    }
+};
+
+pessoa.falar()
+pessoa.soma(5,5)
