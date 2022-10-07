37d954159aeb1829220bee5a32eef7bdc8e175c7
JhowF <jonathanferreirar@gmail.com>
2022-10-07 00:13:57 -0300

    Aula_30_DOM_o_que_é_indrotução_acessando_html_basc

--- Aula_30_DOM_o_que_é/Aula_30_DOM_o_que_é.js
+++ Aula_30_DOM_o_que_é/Aula_30_DOM_o_que_é.js
@@ -2,3 +2,21 @@
 
 //Podemos manipular eventos para afetar o HTML;
 
+var titulo = document.getElementsByTagName('h1')[0];
+
+console.log(titulo.textContent);
+
+
+var lis = document.getElementsByTagName('li');
+
+console.log(lis)
+
+
+var paragrafo = document.getElementsByTagName('paragrafo');
+
+console.log(paragrafo.textContent);
+
+
+var listaItens = document.getElementsByClassName('item');
+
+console.log(listaItens)
