a92168b2672a7bd5d57986b9b978e9764c10d070
JhowF <jonathanferreirar@gmail.com>
2022-10-10 00:45:25 -0300

    Aula_34_Removendo_elementos_DOM_finished

--- Aula_34_Removendo_elementos_DOM/Aula_34_Removendo_elementos_DOM.js
+++ Aula_34_Removendo_elementos_DOM/Aula_34_Removendo_elementos_DOM.js
@@ -1,3 +1,23 @@
 //Removendo Elementos
 
-console.log("")
+
+//Removendo elemento filho
+var container = document.querySelector("#container");
+
+var p = document.querySelector("#container p");
+
+container.removeChild(p);
+
+
+//Removendo elemento filho
+var lista = document.querySelector("#lista");
+
+var li = document.querySelector("#test");
+
+lista.removeChild(li);
+
+
+//Removendo elemento pai
+
+lista2 = document.querySelector("#lista2");
+lista2.remove();
