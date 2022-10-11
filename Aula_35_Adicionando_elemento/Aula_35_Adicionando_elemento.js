bc833813634fcf72e57cf78d35995cc1375d1e01
JhowF <jonathanferreirar@gmail.com>
2022-10-11 00:58:37 -0300

    Aula_35_Adicionando_elemento_finish

--- Aula_35_Adicionando_elemento/Aula_35_Adicionando_elemento.js
+++ Aula_35_Adicionando_elemento/Aula_35_Adicionando_elemento.js
@@ -1,1 +1,39 @@
 //ADICIONANDO ELEMENTO
+
+var elem = document.createElement("div");
+var p = document.createElement("p");
+
+elem.classList = "div-criada";
+//Inserindo elementos
+p.append("Olá humano")  //Usado para adicionar conteúdo
+
+elem.appendChild(p)    //Usado para adicionar tag
+
+
+
+// console.log(elem);
+
+var container = document.querySelector("#container")
+
+container.appendChild(elem)
+
+
+
+
+
+//insertBefore
+
+
+var elem2 = document.createElement("div");
+
+elem2.classList = 'div-Before';
+
+console.log(elem2);
+
+
+
+var elem3 = document.querySelector("#container .div-criada");
+
+console.log(elem3)
+
+container.insertBefore(elem2, elem3);  //elem2 é o que será inserido e o elem3 é a referencia
