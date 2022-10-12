556ba2118427317b7143ae47d563e823408f72e5
JhowF <jonathanferreirar@gmail.com>
2022-10-12 00:56:30 -0300

    Aula_38_Alterando_Css_dom_style.cssText_fontSize

--- Aula_38_Alterando_Css_dom/Aula_38_Alterando_Css_dom.js
+++ Aula_38_Alterando_Css_dom/Aula_38_Alterando_Css_dom.js
@@ -2,3 +2,32 @@
 
 let title = document.querySelector("#title");
 
+title.style.color = 'red';   //font color
+
+title.style.fontSize = '28px';  //font-size
+
+function kiding(){
+title.style.paddingLeft = '70px';}; //padding-left
+
+function backing(){
+    title.style.paddingLeft = '0px';
+};
+
+title.addEventListener('mouseover', kiding);
+
+title.addEventListener('mouseout',back => {title.style.paddingLeft = '0px'});
+
+
+console.log(title)
+
+
+
+//----------------------------
+
+
+let subtitle = ('.title2');
+
+subtitle.style.cssText = 'color: blue; background-color: yellow; font-size:25px;'
+
+
+
