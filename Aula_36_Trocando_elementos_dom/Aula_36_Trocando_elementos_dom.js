3a4275b488188aa4597872454cc953aea8380f4e
JhowF <jonathanferreirar@gmail.com>
2022-10-11 23:59:25 -0300

    Aula_36_Trocando_elementos_dom_finish

--- Aula_36_Trocando_elementos_dom/Aula_36_Trocando_elementos_dom.js
+++ Aula_36_Trocando_elementos_dom/Aula_36_Trocando_elementos_dom.js
@@ -1,2 +1,24 @@
 //TROCANDO ELEMENTOS VIA DOM 
 
+//Montar o elemento que irei alocar
+
+var el = document.createElement('h3');
+
+el.classList = 'Class-put';
+
+var text = document.createTextNode('Texto h3 3 teste');
+
+el.appendChild(text);
+
+console.log(el);
+
+//Selecionar o elemento que quero trocar
+var title = document.querySelector('#title');
+
+console.log(title);
+
+
+//Selecionar o pai do elemento que quero trocar nesse caso por exemplo é o body
+var pai = title.parentNode;
+
+pai.replaceChild(el, title)   //(x, y)    O argumento X irá entrar no lugar do elemento Y
