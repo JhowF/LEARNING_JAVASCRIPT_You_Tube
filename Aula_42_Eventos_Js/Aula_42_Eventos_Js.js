0964a24848a2b7d348059c041d87445eaeaeca47
JhowF <jonathanferreirar@gmail.com>
2022-10-13 00:43:30 -0300

    Aula_42_Eventos_Js_onload

---
+++ Aula_42_Eventos_Js/Aula_42_Eventos_Js.js
@@ -0,0 +1,8 @@
+//Eventos
+
+//Onload  'após o carregamento da pagina'
+
+
+window.onload = function(){   //Usando o onload posso usar o scrip no inicio do html
+    console.log("Carregou")   //Isso porque o scrip só será execultado após o carregamento da janela 
+}
