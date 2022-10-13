c6bbcf0705218a9de472e08fb98efae749d698a1
JhowF <jonathanferreirar@gmail.com>
2022-10-12 22:49:36 -0300

    Aula_39_Propriedades_de_document

--- Aula_39_Propriedades_de_document/Aula_39_Propriedades_de_document.js
+++ Aula_39_Propriedades_de_document/Aula_39_Propriedades_de_document.js
@@ -4,3 +4,13 @@
 
 //Podemos retirar vária informações importantes de suas propriedades também;
 
+console.log(document.body);
+
+console.log(document.links);
+//console.log(document.links[0]);
+
+console.log(document.head);
+
+console.log(document.URL);
+
+console.log(document.title);
