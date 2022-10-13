0d792723295721f8a0715073b859d361dd9f5fe8
JhowF <jonathanferreirar@gmail.com>
2022-10-12 23:30:22 -0300

    Aula_41_setTimrout_setInterval

--- Aula_41_setTimrout_setInterval/Aula_41_setTimrout_setInterval.js
+++ Aula_41_setTimrout_setInterval/Aula_41_setTimrout_setInterval.js
@@ -1,1 +1,8 @@
 //sitTimeout e Setinterval
+
+//Podemos com estas funções criar ações no software que executam
+//depois de um tempo ou de tempos em tempos;
+
+/*Um dos argumentos destas funções é uma callback function;*/
+
+
