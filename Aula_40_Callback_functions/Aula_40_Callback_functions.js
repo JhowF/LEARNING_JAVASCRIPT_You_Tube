a5a5a24106c9bcf62d98dfa3ade4554cb8e1ecc4
JhowF <jonathanferreirar@gmail.com>
2022-10-12 23:26:38 -0300

    Aula_41_setTimrout_setInterval

--- Aula_40_Callback_functions/Aula_40_Callback_functions.js
+++ Aula_40_Callback_functions/Aula_40_Callback_functions.js
@@ -1,1 +1,22 @@
-//CALLBACK FUNCTIONS
+                 //CALLBACK FUNCTIONS
+
+//Amplamente utilizado em JS
+
+
+function exibir (num) {
+    console.log(num);
+};
+
+function soma(a, b, callback) {
+    var op = a + b;
+    callback(op);
+};
+
+function multiplicacao(a, b, cb) {
+    var op = a *b;
+    cb(op);
+};
+
+soma(2, 2, exibir);
+
+multiplicacao(7, 7, exibir);
---
+++ Aula_41_setTimrout_setInterval/Aula_41_setTimrout_setInterval.js
@@ -0,0 +1,1 @@
+//sitTimeout e Setinterval
