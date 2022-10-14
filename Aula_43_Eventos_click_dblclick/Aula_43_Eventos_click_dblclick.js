fe3ebdabe770eb03be578f511b8bbb8ab2914794
JhowF <jonathanferreirar@gmail.com>
2022-10-13 23:42:35 -0300

    Aula_43_Eventos_click_dblclick_this_display:none;

--- Aula_43_Eventos_click_dblclick/Aula_43_Eventos_click_dblclick.js
+++ Aula_43_Eventos_click_dblclick/Aula_43_Eventos_click_dblclick.js
@@ -1,3 +1,30 @@
 var btn = document.querySelector("#btn");
 
-btn.addEventListener('click',)
+btn.addEventListener('click', function() {
+
+    console.log('Clicou');
+
+    console.log(this)
+
+    this.style.cssText = 'color: red; font-size:17px; width:150px;'
+
+});
+
+var title = document.querySelector('#title');
+
+title.addEventListener('click', function(){
+    console.log('Title ok')
+    var btn = document.querySelector("#btn");
+    btn.style.display = 'none';
+    console.log(this)
+
+});
+
+var tile2 = document.querySelector('.title2');
+
+tile2.addEventListener('dblclick', function() {
+
+    console.log("Clique duplo ok");
+    console.log(this)
+
+})
